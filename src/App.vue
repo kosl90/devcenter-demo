7dbaaa8faba9c80f0af8b823438ff7461b39b89a
kosl90 <kos1990l@gmail.com>
2017-02-02 18:30:23 -0800

    skeleon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <section class="header clearfix">
+      <span class="banner"><a href="#"><img src="./assets/logo.png"></a></span>
+      <nav>
+        <ul>
+          <li><a href="#">应用管理</a></li>
+          <li><a href="#">论坛</a></li>
+          <li><a href="#">申请记录</a></li>
+          <li><a href="#">用户名</a></li>
+          <li><a href="#">退出</a></li>
+        </ul>
+      </nav>
+    </section>
+    <section class="slides">
+    this is slide.
+    </section>
+    <section class="news container">
+      <div class="row">
+        <section class="prod-news col-md-4">A</section>
+        <section class="site-news col-md-4">B</section>
+        <section class="forum-news col-md-4">C</section>
+      </div>
+    </section>
+    <footer>
+      This is Footer.
+    </footer>
   </div>
 </template>
 
@@ -16,13 +39,34 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.header {
+  width: 100%;
+  .banner {
+    float: left;
+    img {
+      height: 1.5em;
+    }
+  }
+
+  nav {
+    float: right;
+    ol li, ul li {
+      list-style: none;
+      float: left;
+    }
+  }
+}
+
+.slides {
+  height: 28em;
+  text-align:center;
 }
 </style>
