923b10c914dc20840c43949c9eafaab4f36c7446
kosl90 <kos1990l@gmail.com>
2017-02-21 11:30:38 +0800

    add a possible webpack.server.config.js

--- build/webpack.server.conf.js
+++ build/webpack.server.conf.js
@@ -1,1 +1,39 @@
+var path = require('path');
+var config = require('../config');
+var projectRoot = path.resolve(__dirname, '../');
 
+module.exports = {
+  entry: {
+    server: './server/main.js',
+  },
+  output: {
+    path: config.build.assetsPath,
+    filename: 'server.js',
+  },
+  resolve: {
+    alias: {
+    }
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: [
+          path.join(projectRoot, 'server')
+        ],
+        exclude: /node_modules/
+      },
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: [
+          path.join(projectRoot, 'server')
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+}
