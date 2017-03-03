4bb6f292feb31b6a5aedbbe984f307b6294de9be
kosl90 <kos1990l@gmail.com>
2017-03-03 09:40:14 +0800

    use off-document in test if possible

--- test/unit/specs/FixedZone.spec.js
+++ test/unit/specs/FixedZone.spec.js
@@ -4,11 +4,13 @@ import assert from 'assert';
 
 describe('FixedZone.vue', () => {
   it('should render correct contents', () => {
-    /* eslint-disable no-undef */
+    // Two ways to test a component. Because FixedZone is not an constructor.
+    // Use `$mount` to render component off-document.
+    // const TestComponent = Vue.extend(FixedZone);
+    // const vm = new TestComponent().$mount();
     const vm = new Vue({
-      el: document.createElement('div'),
       render: h => h(FixedZone),
-    });
+    }).$mount();
     const actTxt = vm.$el.querySelector('.fixed-zone a:first-child').textContent;
     assert.equal(actTxt, '问题反馈');
   });
